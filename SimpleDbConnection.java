a62de16052b850d158403b9c1317bd311f5a8104
naidooabel3-lang <naidooabel3@gmail.com>
2026-04-13 13:07:35 +0200

    Update SimpleDbConnection.java

--- SimpleDbConnection.java
+++ SimpleDbConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SimpleDbConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/healthfirst";
+    private static final String URL = "ADD HERE";
     private static final String USER = "root";
-    private static final String PASS = "Admin123";
+    private static final String PASS = "ADD HERE";
 
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(URL, USER, PASS)) {
